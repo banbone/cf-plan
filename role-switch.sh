6c51f8bd6ce95827fe4f6d775bd31b3d09c5dc26
banbone <danieljjones@outlook.com>
2022-04-05 08:53:43 +0100

    updates to role switch script

--- role-switch.sh
+++ role-switch.sh
@@ -1,2 +1,6 @@
 #! /bin/sh
-aws sts get-caller-identity
+aws sts get-caller-identity > caller-id
+account=$(jq -r '.Account' caller-id)
+role="arn:aws:iam::$account:role/$1"
+aws sts assume-role --role-arn "$role" \
+	--session-name cf-plan
